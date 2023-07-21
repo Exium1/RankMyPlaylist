950cac5999cd2e7f91c4178722f3954a84cd81ac
Exium <exium16@gmail.com>
2023-07-20 20:54:09 -0700

    Randomized tracks

--- server/utils/sorting.js
+++ server/utils/sorting.js
@@ -26,6 +26,26 @@ let sortingData = {
 	}
 };
 
+function shuffle(array) {
+	let currentIndex = array.length,
+		randomIndex;
+
+	// While there remain elements to shuffle.
+	while (currentIndex != 0) {
+		// Pick a remaining element.
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+
+		// And swap it with the current element.
+		[array[currentIndex], array[randomIndex]] = [
+			array[randomIndex],
+			array[currentIndex]
+		];
+	}
+
+	return array;
+}
+
 const initSorting = (sessionID) => {
 	const playlistData = database.getCurrentPlaylist(sessionID);
 
@@ -33,6 +53,8 @@ const initSorting = (sessionID) => {
 	if (sortingData[sessionID] != null) return 401;
 
 	const tracks = playlistData.tracks.items.map((item) => item.track.id);
+
+	shuffle(tracks);
 
 	sortingData[sessionID] = {
 		mergeSort: {
